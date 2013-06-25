febfce52a11af754594bdad20597dfc1abed08c4
Adrián García Santiago <adrian@linux-trio.site>
2013-06-25 19:24:11 +0200

    Añadido soporte básico a varios jugadores.

--- gameClient.js
+++ gameClient.js
@@ -6,8 +6,9 @@ var ctx=canvas.getContext('2d');
 //var mousex=0,mousey=0;
 var lastKey=null;
 var PRESSING=[];
-var player=new Circle(50,50,10);
-var pelota=new Circle(100,100,5);
+var radio_player = 10, radio_pelota = 5;
+var player = [];
+var pelota = { x: 0, y: 0 };
 
 
 window.requestAnimationFrame = requestAnimationFrame || mozRequestAnimationFrame || webkitmozRequestAnimationFrame;
@@ -15,10 +16,8 @@ window.requestAnimationFrame = requestAnimationFrame || mozRequestAnimationFrame
 var ws = new WebSocket('ws://localhost:8090/room?width='+canvas.width+'&height='+canvas.height);
 ws.onmessage = function(msg){
 	var data = JSON.parse(msg.data);
-	player.x = data.player.x;
-	player.y = data.player.y;
-	pelota.x = data.pelota.x;
-	pelota.y = data.pelota.y;
+	player = data.player;
+	pelota = data.pelota;
 }
 ws.onclose = function(data){console.log(data);}
 
@@ -38,23 +37,19 @@ ws.onopen = function() {console.log("Conexion establecida");
 };
 
 
+function paint_circle(pos, radio, color) {
+	ctx.strokeStyle = color;
+	ctx.beginPath();
+	ctx.arc(pos.x,pos.y,radio,0,Math.PI*2,true);
+	ctx.stroke();
+}
+
 function paint(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.strokeStyle='#0f0';
-    ctx.beginPath();
-    ctx.arc(player.x,player.y,player.radius,0,Math.PI*2,true);
-    ctx.stroke();
-    ctx.strokeStyle='#f00';
-    ctx.beginPath();
-    ctx.arc(pelota.x,pelota.y,pelota.radius,0,Math.PI*2,true);
-    ctx.stroke();
+	for(var i in player)
+		paint_circle(player[i], radio_player, '#0f0');
+	paint_circle(pelota, radio_pelota, '#0f0');
 	
 	requestAnimationFrame(paint);
 }
 requestAnimationFrame(paint);
-
-function Circle(x,y,radius){
-    this.x=(x==null)?0:x;
-    this.y=(y==null)?0:y;
-    this.radius=(radius==null)?0:radius;
-}
--- server.rb
+++ server.rb
@@ -6,6 +6,7 @@ PORT = 8090
 REFRESH_TIME = 0.05
 ACELERACION = 0.5
 MAX_VEL = 6
+WIDTH,HEIGHT = 600,300
 
 RADIO_PLAYER = 10
 RADIO_PELOTA = 5
@@ -14,56 +15,81 @@ def distancia obj1, obj2
 	Math.sqrt((obj1[:x]-obj2[:x])**2 + (obj1[:y]-obj2[:y])**2)
 end
 
+$pos = { player: [], pelota: { x: 60, y: 60 } }
+$vel = { player: [], pelota: { x: 0, y: 0 } }
+$apretada = []
+$njugadores = 0
+$timer = nil
+$clients = Hash.new # clients[connection] = pos
+
+def send_all data
+	$clients.each_key { |ws| ws.send data }
+end
 
 EventMachine::run do
 	EventMachine::WebSocket.run(:host => "0.0.0.0", :port => PORT) do |connection|
-		width,height = 100,100
-		pos = { player: { x: 20, y: 20 }, pelota: { x: 60, y: 60 } }
-		vel = { player: { x: 0, y: 0 }, pelota: { x: 0, y: 0 } }
-		apretada = {}
-		timer = nil
+		cpos = { x: 20, y: 20 }
+		cvel = { x: 0, y: 0 }
+		capr = {}
 		
 		connection.onopen do |handshake| puts "Cliente conectado! :D"
-			width,height = handshake.query.values_at('width', 'height').map{|e| e.to_i }
-			# TODO  pos inicial
-			timer = EventMachine::PeriodicTimer.new(REFRESH_TIME) do
-				vel[:player][:x] += (apretada[37] ? -ACELERACION : 0) + (apretada[39] ? ACELERACION : 0)
-				vel[:player][:y] += (apretada[38] ? -ACELERACION : 0) + (apretada[40] ? ACELERACION : 0)
-				for eje,t1,t2 in [[:x,37,39],[:y,38,40]]
-					if apretada[t1] and not apretada[t2]
-						vel[:player][eje] -= ACELERACION if vel[:player][eje]
-						vel[:player][eje] = -MAX_VEL if vel[:player][eje] < -MAX_VEL
-					elsif not apretada[t1] and apretada[t2]
-						vel[:player][eje] += ACELERACION if vel[:player][eje]
-						vel[:player][eje] = MAX_VEL if vel[:player][eje] > MAX_VEL
-					elsif vel[:player][eje] > 0
-						vel[:player][eje] -= ACELERACION
-						vel[:player][eje] = 0 if vel[:player][eje] < 0
-					elsif vel[:player][eje] < 0
-						vel[:player][eje] += ACELERACION
-						vel[:player][eje] = 0 if vel[:player][eje] > 0
-					end
-				end
-				
-				for eje,maxeje in [[:x,width],[:y,height]]
-					puts "eje y: #{pos[:player][:y]} #{vel[:player][:y]}    maxeje: #{maxeje}" if eje == :y
-					pos[:player][eje] += vel[:player][eje]
-					pos[:player][eje], vel[:player][eje] = RADIO_PLAYER, 0 if pos[:player][eje] < RADIO_PLAYER
-					pos[:player][eje], vel[:player][eje] = maxeje-RADIO_PLAYER, 0 if pos[:player][eje] > maxeje-RADIO_PLAYER
-				end
-				
-				connection.send JSON.generate(pos)
-			end
+			$clients[connection] = $njugadores
+			$njugadores += 1
+			# TODO  no superponer a otro jugador del campo la posición inicial
+			$pos[:player] << cpos
+			$vel[:player] << cvel
+			$apretada << capr
 		end
 		
 		connection.onmessage do |data| puts data
 			tecla,pressed = data.split
-			apretada[tecla.to_i] = (pressed == '1')
+			capr[tecla.to_i] = (pressed == '1')
 		end
 		
 		connection.onclose do puts "Se fue... T_T"
-			timer.cancel rescue nil
+			i = $clients[connection]
+			$clients.delete connection
+			for cl,icl in $clients
+				$clients[cl] = icl-1 if icl > i
+			end
+			$pos[:player].delete_at i
+			$vel[:player].delete_at i
+			$apretada.delete_at i
+			$njugadores -= 1
 		end
+	end
+	
+	
+	$timer = EventMachine::PeriodicTimer.new(REFRESH_TIME) do
+		
+		$njugadores.times do |i|
+			ipos, ivel, iapr = $pos[:player][i], $vel[:player][i], $apretada[i]
+			
+			for eje,t1,t2 in [[:x,37,39],[:y,38,40]]
+				if iapr[t1] and not iapr[t2]
+					ivel[eje] -= ACELERACION if ivel[eje]
+					ivel[eje] = -MAX_VEL if ivel[eje] < -MAX_VEL
+				elsif not iapr[t1] and iapr[t2]
+					ivel[eje] += ACELERACION if ivel[eje]
+					ivel[eje] = MAX_VEL if ivel[eje] > MAX_VEL
+				elsif ivel[eje] > 0
+					ivel[eje] -= ACELERACION
+					ivel[eje] = 0 if ivel[eje] < 0
+				elsif ivel[eje] < 0
+					ivel[eje] += ACELERACION
+					ivel[eje] = 0 if ivel[eje] > 0
+				end
+			end
+			
+			for eje,maxeje in [[:x,WIDTH],[:y,HEIGHT]]
+				ipos[eje] += ivel[eje]
+				ipos[eje], ivel[eje] = RADIO_PLAYER, 0 if ipos[eje] < RADIO_PLAYER
+				ipos[eje], ivel[eje] = maxeje-RADIO_PLAYER, 0 if ipos[eje] > maxeje-RADIO_PLAYER
+			end
+			
+		end
+		
+		send_all JSON.generate($pos)
 	end
 	
 	puts "Iniciado servidor en el puerto #{PORT}"
