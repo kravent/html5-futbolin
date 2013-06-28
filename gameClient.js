86e7b2240e959d73288b6f02327c0cc2b173194b
Adrián García Santiago <adrian@linux-trio.site>
2013-06-28 14:14:04 +0200

    Mejorado el dibujo
    Dibujado separado por funciones según finalidad.
    Colores! :D

--- gameClient.js
+++ gameClient.js
@@ -3,53 +3,89 @@ var canvas=document.getElementById('canvas');
 canvas.style.background='#000';
 var ctx=canvas.getContext('2d');
 
-//var mousex=0,mousey=0;
 var lastKey=null;
-var PRESSING=[];
+var pressing=[];
 var radio_player = 10, radio_pelota = 5;
 var player = [];
 var pelota = { x: 0, y: 0 };
 
-
 window.requestAnimationFrame = requestAnimationFrame || mozRequestAnimationFrame || webkitmozRequestAnimationFrame;
 
-var ws = new WebSocket('ws://localhost:8090/room?width='+canvas.width+'&height='+canvas.height);
-ws.onmessage = function(msg){
-	var data = JSON.parse(msg.data);
-	player = data.player;
-	pelota = data.pelota;
+var ws = null;
+
+// Conexiones con el servidor
+
+function begin_websocket(server) {
+	ws = new WebSocket('ws://'+server+'/room?width='+canvas.width+'&height='+canvas.height);
+	ws.onmessage = function(msg){
+		var data = JSON.parse(msg.data);
+		player = data.player;
+		pelota = data.pelota;
+	}
+	ws.onclose = function(data){
+		console.log(data);
+		ws = null;
+	}
+
+	ws.onopen = function() {console.log("Conexion establecida");
+		document.addEventListener('keydown',function(evt){
+			lastKey=evt.keyCode;
+			if(!pressing[lastKey]){
+				pressing[evt.keyCode]=true;
+				ws.send(lastKey+' 1');
+			}
+		},false);
+
+		document.addEventListener('keyup',function(evt){
+			pressing[evt.keyCode]=false;
+			ws.send(evt.keyCode+' 0');
+		},false);
+	};
 }
-ws.onclose = function(data){console.log(data);}
-
-ws.onopen = function() {console.log("Conexion establecida");
-	document.addEventListener('keydown',function(evt){
-		lastKey=evt.keyCode;
-		if(!PRESSING[lastKey]){
-			PRESSING[evt.keyCode]=true;
-			ws.send(lastKey+' 1');
-		}
-	},false);
-
-	document.addEventListener('keyup',function(evt){
-		PRESSING[evt.keyCode]=false;
-		ws.send(evt.keyCode+' 0');
-	},false);
-};
 
 
-function paint_circle(pos, radio, color) {
-	ctx.strokeStyle = color;
+
+// Funciones de dibujo
+
+function paint_clear() {
+	ctx.fillStyle = '#1A4300';
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function paint_player(pos) {
+	ctx.strokeStyle = '#000';
+	ctx.fillStyle = '#f00';
 	ctx.beginPath();
-	ctx.arc(pos.x,pos.y,radio,0,Math.PI*2,true);
+	ctx.arc(pos.x,pos.y,radio_player,0,Math.PI*2,true);
+	ctx.fill();
+	ctx.stroke();
+}
+
+function paint_pelota(pos, radio, color) {
+	ctx.strokeStyle = '#000';
+	ctx.fillStyle = '#fff';
+	ctx.beginPath();
+	ctx.arc(pos.x,pos.y,radio_pelota,0,Math.PI*2,true);
+	ctx.fill();
 	ctx.stroke();
 }
 
 function paint(){
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    paint_clear();
 	for(var i in player)
-		paint_circle(player[i], radio_player, '#0f0');
-	paint_circle(pelota, radio_pelota, '#0f0');
+		paint_player(player[i]);
+	paint_pelota(pelota);
 	
 	requestAnimationFrame(paint);
 }
-requestAnimationFrame(paint);
+
+function begin_paint() {
+	requestAnimationFrame(paint);
+}
+
+
+
+// Inicializacion de componentes
+
+begin_paint();
+begin_websocket('localhost:8090');
