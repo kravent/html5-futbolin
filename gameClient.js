83512ea4b2755ef9a35264d6dcece494a29636f3
Adrián García <dev@agaman.me>
2013-07-08 20:39:49 +0200

    Panel de conexión.
    Pregunta el servidor y el puerto al que conectarse.

--- gameClient.js
+++ gameClient.js
@@ -3,6 +3,12 @@ var canvas=document.getElementById('canvas');
 canvas.style.background='#000';
 var ctx=canvas.getContext('2d');
 
+var paneles = {
+	connect: document.getElementById('div_connect'),
+	connecting: document.getElementById('div_connecting'),
+	game: document.getElementById('div_game')
+};
+
 var lastKey=null;
 var pressing=[];
 var radio_player = 10, radio_pelota = 5;
@@ -10,24 +16,27 @@ var player = [];
 var pelota = { x: 0, y: 0 };
 
 window.requestAnimationFrame = requestAnimationFrame || mozRequestAnimationFrame || webkitmozRequestAnimationFrame;
-
 var ws = null;
+
+
+// Manejo de paneles
+
+function show_panel(name) {
+	for(var i in paneles) {
+		if(i == name) paneles[i].style.display = "";
+		else paneles[i].style.display = "none";
+	}
+}
+
 
 // Conexiones con el servidor
 
 function begin_websocket(server) {
+	show_panel('connecting');
 	ws = new WebSocket('ws://'+server+'/room?width='+canvas.width+'&height='+canvas.height);
-	ws.onmessage = function(msg){
-		var data = JSON.parse(msg.data);
-		player = data.player;
-		pelota = data.pelota;
-	}
-	ws.onclose = function(data){
-		console.log(data);
-		ws = null;
-	}
 
-	ws.onopen = function() {console.log("Conexion establecida");
+	ws.onopen = function() {
+		show_panel('game');
 		document.addEventListener('keydown',function(evt){
 			lastKey=evt.keyCode;
 			if(!pressing[lastKey]){
@@ -40,6 +49,18 @@ function begin_websocket(server) {
 			pressing[evt.keyCode]=false;
 			ws.send(evt.keyCode+' 0');
 		},false);
+	};
+	
+	ws.onmessage = function(msg){
+		var data = JSON.parse(msg.data);
+		player = data.player;
+		pelota = data.pelota;
+	}
+	
+	ws.onclose = function(data){
+		show_panel('connect');
+		console.log(data);
+		ws = null;
 	};
 }
 
@@ -84,8 +105,19 @@ function begin_paint() {
 }
 
 
+// Manejador de formularios
+
+function on_submit_connect() {
+	var form = document.forms['connect'];
+	var server = form['server'].value + ':' + form['port'].value;
+	begin_websocket(server);
+	return false;
+}
+
 
 // Inicializacion de componentes
 
+show_panel('connect');
 begin_paint();
-begin_websocket('localhost:8090');
+document.forms['connect'].onsubmit = on_submit_connect;
+
